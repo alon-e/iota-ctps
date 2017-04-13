bbc5282d504c9f91fc768696122c09283b4449cf
Alon Elmaliah <alon.elmaliah@gmail.com>
2017-04-13 23:53:23 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='iota-ctps',
+    version='1.0',
+    packages=[ '' ],
+    install_requires=['networkx', 'terminaltables', 'iota'],
+    url='',
+    license='',
+    author='alon-e',
+    author_email='alon.elmaliah@gmail.com',
+    description=''
+)
