057d4544a4b103a9d5b44797ddea70a28b650dc4
Alon Elmaliah <alon.elmaliah@gmail.com>
2017-04-14 22:23:51 +0300

    added broadcast to API

--- ctps.py
+++ ctps.py
@@ -6,6 +6,26 @@ import time
 import sys
 from terminaltables import AsciiTable
 
+import urllib2
+import json
+
+TIMEOUT = 7
+
+def API(request,auth,url):
+
+    try:
+        stringified = json.dumps(request)
+        headers = {'content-type': 'application/json', 'Authorization': auth}
+        request = urllib2.Request(url=url, data=stringified, headers=headers)
+        returnData = urllib2.urlopen(request,timeout=TIMEOUT).read()
+
+        response = json.loads(returnData)
+        return response
+    except:
+        print url,"Timeout!"
+        pass
+
+
 class transaction:
     def __init__(self,tryte_string, hash_string):
         self.hash = hash_string
@@ -24,7 +44,7 @@ class transaction:
 class tangle:
 
 
-    def __init__(self,path,resolution):
+    def __init__(self,path,resolution,auth_key,api_url):
 
         self.directory = path
         self.output = './table.out'
@@ -46,6 +66,10 @@ class tangle:
 
         self.broadcast_max_tps = 0
         self.broadcast_max_ctps = 0
+
+        self.auth_key = auth_key
+        self.api_url = api_url
+
 
     def add_tx_to_tangle(self, tx):
 
@@ -201,14 +225,17 @@ class tangle:
         }
         with open('feed.out', 'w+') as f:
             f.write(str(json))
+        if self.auth_key:
+            res = API(json,self.auth_key,api_url)
+            print res
         pass
 
 
 
 
 
-def main(path,resolution):
-    t = tangle(path,resolution)
+def main(path,resolution,auth_key,api_url):
+    t = tangle(path,resolution,auth_key,api_url)
     while True:
         t.incremental_read()
         t.print_stats()
@@ -217,8 +244,19 @@ def main(path,resolution):
 if __name__ == '__main__':
 
     if len(sys.argv) <3:
-        print 'usage: ctps.py [path_to_export] [sample_interval]'
+        print 'usage: ctps.py [path_to_export] [sample_interval] (auth_key) (url)'
         exit(1)
 
+    if len(sys.argv) <4:
+        auth_key = None
+        api_url = None
+    elif len(sys.argv) <5:
+        auth_key = sys.argv[3]
+    else:
+        auth_key = sys.argv[3]
+        api_url = sys.argv[4]
 
-    main(sys.argv[1],sys.argv[2])
+
+
+
+    main(sys.argv[1],sys.argv[2],auth_key,api_url)
